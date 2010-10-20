cfb7135d21a0b20c54f46c949f0527543de6af11
Smasty <martin@smasty.net>
2010-10-20 19:11:51 +0200

    test db change

--- tests/db.sql
+++ tests/db.sql
@@ -1,22 +1,22 @@
 CREATE TABLE `author` (
-  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `id` tinyint(4) NOT NULL,
   `name` varchar(256) COLLATE utf8_bin NOT NULL,
   `web` varchar(256) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 INSERT INTO `author` (`id`, `name`, `web`) VALUES
 (11,	'Martin Srank',	'http://smasty.net'),
 (12,	'Linus Torvalds',	'http://torvalds-family.blogspot.com');
 
 CREATE TABLE `software` (
-  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `id` tinyint(4) NOT NULL,
   `aid` tinyint(4) NOT NULL,
   `title` varchar(256) COLLATE utf8_bin NOT NULL,
   `web` varchar(256) COLLATE utf8_bin NOT NULL,
   `slogan` varchar(256) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 INSERT INTO `software` (`id`, `aid`, `title`, `web`, `slogan`) VALUES
 (1,	11,	'Neevo',	'http://neevo.smasty.net',	''),
