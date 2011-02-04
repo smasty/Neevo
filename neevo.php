276c9ea430f2c718a27660437281132d72432782
Smasty <martin@smasty.net>
2011-02-04 21:50:53 +0100

    quiet test mode

--- neevo.php
+++ neevo.php
@@ -61,7 +61,7 @@ class Neevo implements INeevoObservable, INeevoObserver {
   public static $defaultDriver = 'mysql';
 
   // Neevo revision
-  const REVISION = 345;
+  const REVISION = 346;
 
   // Data types
   const BOOL = 'b';
