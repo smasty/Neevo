bb1cdebdb6172fb1314f997156efeb778d01322f
Smasty <martin@smasty.net>
2011-02-08 22:20:45 +0100

    method rename

--- neevo.php
+++ neevo.php
@@ -61,7 +61,7 @@ class Neevo implements INeevoObservable, INeevoObserver {
   public static $defaultDriver = 'mysql';
 
   // Neevo revision
-  const REVISION = 356;
+  const REVISION = 359;
 
   // Data types
   const BOOL = 'b';
