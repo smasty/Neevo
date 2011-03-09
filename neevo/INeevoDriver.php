256053317edf52ccdd7f594ce48369bdb99855b1
Smasty <martin@smasty.net>
2011-03-09 21:34:12 +0100

    Indentation

--- neevo/INeevoDriver.php
+++ neevo/INeevoDriver.php
@@ -8,7 +8,7 @@
  * with this package in the file license.txt.
  *
  * @author   Martin Srank (http://smasty.net)
- * @license  http://neevo.smasty.net/license  MIT license
+ * @license  http://neevo.smasty.net/license MIT license
  * @link     http://neevo.smasty.net/
  *
  */
@@ -49,131 +49,149 @@
 interface INeevoDriver {
 
 
-  /**
-   * Check for required PHP extension.
-   * @return void
-   * @throws NeevoException
-   */
-  public function  __construct();
+    /**
+     * Check for required PHP extension.
+     * @return void
+     * @throws NeevoException
+     */
+    public function __construct();
 
-  /**
-   * Create connection to database.
-   * @param array $config Configuration options
-   * @return void
-   */
-  public function connect(array $config);
 
-  /**
-   * Close the connection.
-   * @return void
-   */
-  public function close();
+    /**
+     * Create connection to database.
+     * @param array $config Configuration options
+     * @return void
+     */
+    public function connect(array $config);
 
-  /**
-   * Free memory used by given result.
-   * @param resource $resultSet
-   * @return bool
-   */
-  public function free($resultSet);
 
-  /**
-   * Execute given SQL statement.
-   * @param string $queryString
-   * @return resource|bool
-   */
-  public function query($queryString);
+    /**
+     * Close the connection.
+     * @return void
+     */
+    public function close();
 
-  /**
-   * Begin a transaction if supported.
-   * @param string $savepoint
-   * @return void
-   */
-  public function begin($savepoint = null);
 
-  /**
-   * Commit statements in a transaction.
-   * @param string $avepoint
-   * @return void
-   */
-  public function commit($savepoint = null);
+    /**
+     * Free memory used by given result.
+     * @param resource $resultSet
+     * @return bool
+     */
+    public function free($resultSet);
 
-  /**
-   * Rollback changes in a transaction.
-   * @param string $savepoint
-   * @return void
-   */
-  public function rollback($savepoint = null);
 
-  /**
-   * Fetch row from given result set as an associative array.
-   * @param resource $resultSet
-   * @return array
-   */
-  public function fetch($resultSet);
+    /**
+     * Execute given SQL statement.
+     * @param string $queryString
+     * @return resource|bool
+     */
+    public function query($queryString);
 
-  /**
-   * Move internal result pointer.
-   * @param resource $resultSet
-   * @param int $offset
-   * @return bool
-   */
-  public function seek($resultSet, $offset);
 
-  /**
-   * Get the ID generated in the INSERT statement.
-   * @return int
-   */
-  public function insertId();
+    /**
+     * Begin a transaction if supported.
+     * @param string $savepoint
+     * @return void
+     */
+    public function begin($savepoint = null);
 
-  /**
-   * Randomize result order.
-   * @param NeevoStmtBase $statement
-   * @return void
-   */
-  public function rand(NeevoStmtBase $statement);
 
-  /**
-   * Get the number of rows in the given result set.
-   * @param resource $resultSet
-   * @return int|FALSE
-   */
-  public function rows($resultSet);
+    /**
+     * Commit statements in a transaction.
+     * @param string $avepoint
+     * @return void
+     */
+    public function commit($savepoint = null);
 
-  /**
-   * Get the number of affected rows in previous operation.
-   * @return int
-   */
-  public function affectedRows();
 
-  /**
-   * Escape given value.
-   * @param mixed $value
-   * @param string $type
-   * @return mixed
-   */
-  public function escape($value, $type);
+    /**
+     * Rollback changes in a transaction.
+     * @param string $savepoint
+     * @return void
+     */
+    public function rollback($savepoint = null);
 
-  /**
-   * Decode given value.
-   * @param mixed $value
-   * @param string $type
-   * @return mixed
-   */
-  public function unescape($value, $type);
 
-  /**
-   * Get the PRIMARY KEY column for given table.
-   * @param string $table
-   * @return string|NULL
-   */
-  public function getPrimaryKey($table);
+    /**
+     * Fetch row from given result set as an associative array.
+     * @param resource $resultSet
+     * @return array
+     */
+    public function fetch($resultSet);
 
-  /**
-   * Get types of columns in given result set.
-   * @param resource $resultSet
-   * @param string $table
-   * @return array
-   */
-  public function getColumnTypes($resultSet, $table);
-  
+
+    /**
+     * Move internal result pointer.
+     * @param resource $resultSet
+     * @param int $offset
+     * @return bool
+     */
+    public function seek($resultSet, $offset);
+
+
+    /**
+     * Get the ID generated in the INSERT statement.
+     * @return int
+     */
+    public function insertId();
+
+
+    /**
+     * Randomize result order.
+     * @param NeevoStmtBase $statement
+     * @return void
+     */
+    public function rand(NeevoStmtBase $statement);
+
+
+    /**
+     * Get the number of rows in the given result set.
+     * @param resource $resultSet
+     * @return int|FALSE
+     */
+    public function rows($resultSet);
+
+
+    /**
+     * Get the number of affected rows in previous operation.
+     * @return int
+     */
+    public function affectedRows();
+
+
+    /**
+     * Escape given value.
+     * @param mixed $value
+     * @param string $type
+     * @return mixed
+     */
+    public function escape($value, $type);
+
+
+    /**
+     * Decode given value.
+     * @param mixed $value
+     * @param string $type
+     * @return mixed
+     */
+    public function unescape($value, $type);
+
+
+    /**
+     * Get the PRIMARY KEY column for given table.
+     * @param string $table
+     * @return string|NULL
+     */
+    public function getPrimaryKey($table);
+
+
+    /**
+     * Get types of columns in given result set.
+     * @param resource $resultSet
+     * @param string $table
+     * @return array
+     */
+    public function getColumnTypes($resultSet, $table);
+
+
 }
--- neevo/NeevoConnection.php
+++ neevo/NeevoConnection.php
@@ -8,7 +8,7 @@
  * with this package in the file license.txt.
  *
  * @author   Martin Srank (http://smasty.net)
- * @license  http://neevo.smasty.net/license  MIT license
+ * @license  http://neevo.smasty.net/license MIT license
  * @link     http://neevo.smasty.net/
  *
  */
@@ -30,211 +30,225 @@
  */
 class NeevoConnection implements INeevoObservable {
 
-  /** @var array */
-  private $config;
 
-  /** @var bool */
-  private $connected = false;
+    /** @var array */
+    private $config;
 
-  /** @var INeevoDriver */
-  private $driver;
+    /** @var bool */
+    private $connected = false;
 
-  /** @var NeevoStmtParser */
-  private $stmtParser;
+    /** @var INeevoDriver */
+    private $driver;
 
-  /** @var SplObjectStorage */
-  private $observers;
+    /** @var NeevoStmtParser */
+    private $stmtParser;
 
-  /** @var INeevoCache */
-  private $cache;
+    /** @var SplObjectStorage */
+    private $observers;
 
-  /**
-   * Establish a connection.
-   * @param array|string|Traversable $config
-   * @param INeevoCache $cache
-   * @return void
-   * @throws InvalidArgumentException
-   */
-  public function __construct($config, INeevoCache $cache = null){
-    $this->observers = new SplObjectStorage;
-
-    $this->cache = $cache ? $cache : (defined('NETTE') ? new NeevoCacheNette : new NeevoCache);
-    
-    // Parse
-    if(is_string($config)){
-      parse_str($config, $config);
-    }
-    elseif($config instanceof Traversable){
-      $config = iterator_to_array($config);
-    }
-    elseif(!is_array($config)){
-      throw new InvalidArgumentException('Configuration must be an array, string or instance of Traversable.');
-    }
-
-    // Defaults
-    $defaults = array(
-      'driver' => Neevo::$defaultDriver,
-      'lazy' => false,
-      'tablePrefix' => '',
-      'formatDateTime' => '',
-      'detectTypes' => false,
-      'rowClass' => 'NeevoRow',
-      'observer' => null
-    );
-
-    // Aliases
-    self::alias($config, 'driver', 'extension');
-    self::alias($config, 'username', 'user');
-    self::alias($config, 'password', 'pass');
-    self::alias($config, 'password', 'pswd');
-    self::alias($config, 'host', 'hostname');
-    self::alias($config, 'host', 'server');
-    self::alias($config, 'database', 'db');
-    self::alias($config, 'database', 'dbname');
-    self::alias($config, 'tablePrefix', 'table_prefix');
-    self::alias($config, 'tablePrefix', 'prefix');
-    self::alias($config, 'charset', 'encoding');
-    self::alias($config, 'observer', 'profiler');
-
-    $config += $defaults;
-
-    $this->setDriver($config['driver']);
-
-    if($config['observer'] instanceof INeevoObserver){
-      $this->attachObserver($config['observer']);
-    }
-
-    $this->config = $config;
-
-    if($config['lazy'] === false){
-      $this->realConnect();
-    }
-  }
-
-  /**
-   * Get configuration.
-   * @param string $key
-   * @return mixed
-   */
-  public function getConfig($key = null){
-    if($key === null){
-      return $this->config;
-    }
-    return isset($this->config[$key]) ? $this->config[$key] : null;
-  }
-
-  /**
-   * Get defined table prefix.
-   * @return string
-   */
-  public function prefix(){
-    return isset($this->config['tablePrefix'])
-      ? $this->config['tablePrefix'] : '';
-  }
-
-  /** @return INeevoDriver */
-  public function driver(){
-    return $this->driver;
-  }
-
-  /** @return NeevoStmtParser */
-  public function stmtParser(){
-    return $this->stmtParser;
-  }
-
-  /** @return INeevoCache */
-  public function cache(){
-    return $this->cache;
-  }
-
-  /**
-   * Create an alias for configuration value.
-   * @param array $config Passed by reference
-   * @param string $key
-   * @param string $alias Alias of $key
-   * @return void
-   */
-  public static function alias(&$config, $key, $alias){
-    if(isset($config[$alias]) && !isset($config[$key])){
-      $config[$key] = $config[$alias];
-      //unset($config[$alias]);
-    }
-  }
-
-  /**
-   * Attach given observer.
-   * @param INeevoObserver $observer
-   * @return void
-   */
-  public function attachObserver(INeevoObserver $observer){
-      $this->observers->attach($observer);
-  }
-
-  /**
-   * Detach given observer.
-   * @param INeevoObserver $observer
-   * @return void
-   */
-  public function detachObserver(INeevoObserver $observer){
-    $this->observers->detach($observer);
-  }
-
-  /**
-   * Notify observers.
-   * @param int $event
-   * @param NeevoStmtBase $statement
-   * @return void
-   */
-  public function notifyObservers($event, NeevoStmtBase $statement = null){
-    foreach($this->observers as $observer){
-      $observer->updateStatus($this, $event, $statement);
-    }
-  }
+    /** @var INeevoCache */
+    private $cache;
 
 
-  /*  ************  Internal methods ************  */
+    /**
+     * Establish a connection.
+     * @param array|string|Traversable $config
+     * @param INeevoCache $cache
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    public function __construct($config, INeevoCache $cache = null){
+        $this->observers = new SplObjectStorage;
 
-  
-  /** @internal */
-  public function realConnect(){
-    if($this->connected === false){
-      $this->driver->connect($this->config);
-      $this->connected = true;
+        $this->cache = $cache ? $cache : (defined('NETTE') ? new NeevoCacheNette : new NeevoCache);
 
-      $this->notifyObservers(INeevoObserver::CONNECT);
-    }
-  }
+        // Parse
+        if(is_string($config)){
+            parse_str($config, $config);
+        }
+        elseif($config instanceof Traversable){
+            $config = iterator_to_array($config);
+        }
+        elseif(!is_array($config)){
+            throw new InvalidArgumentException('Configuration must be an array, string or instance of Traversable.');
+        }
 
-  private function isDriver($class){
-    return (class_exists($class) && in_array('INeevoDriver', class_implements($class)));
-  }
+        // Defaults
+        $defaults = array(
+            'driver' => Neevo::$defaultDriver,
+            'lazy' => false,
+            'tablePrefix' => '',
+            'formatDateTime' => '',
+            'detectTypes' => false,
+            'rowClass' => 'NeevoRow',
+            'observer' => null
+        );
 
-  /**
-   * Set the driver and statement parser.
-   * @param string $driver
-   * @return void
-   * @throws NeevoException
-   */
-  private function setDriver($driver){
-    $class = "NeevoDriver$driver";
+        // Aliases
+        self::alias($config, 'driver', 'extension');
+        self::alias($config, 'username', 'user');
+        self::alias($config, 'password', 'pass');
+        self::alias($config, 'password', 'pswd');
+        self::alias($config, 'host', 'hostname');
+        self::alias($config, 'host', 'server');
+        self::alias($config, 'database', 'db');
+        self::alias($config, 'database', 'dbname');
+        self::alias($config, 'tablePrefix', 'table_prefix');
+        self::alias($config, 'tablePrefix', 'prefix');
+        self::alias($config, 'charset', 'encoding');
+        self::alias($config, 'observer', 'profiler');
 
-    if(!$this->isDriver($class)){
-      include_once dirname(__FILE__) . '/drivers/'.strtolower($driver).'.php';
+        $config += $defaults;
 
-      if(!$this->isDriver($class)){
-        throw new NeevoException("Unable to create instance of Neevo driver '$driver'.");
-      }
+        $this->setDriver($config['driver']);
+
+        if($config['observer'] instanceof INeevoObserver){
+            $this->attachObserver($config['observer']);
+        }
+
+        $this->config = $config;
+
+        if($config['lazy'] === false){
+            $this->realConnect();
+        }
     }
 
-    $this->driver = new $class;
 
-    // Set statement parser
-    if(in_array('NeevoStmtParser', class_parents($class))){
-      $this->stmtParser = $this->driver;
+    /**
+     * Get configuration.
+     * @param string $key
+     * @return mixed
+     */
+    public function getConfig($key = null){
+        if($key === null){
+            return $this->config;
+        }
+        return isset($this->config[$key]) ? $this->config[$key] : null;
     }
-    else{
-      $this->stmtParser = new NeevoStmtParser;
+
+
+    /**
+     * Get defined table prefix.
+     * @return string
+     */
+    public function prefix(){
+        return isset($this->config['tablePrefix'])
+            ? $this->config['tablePrefix'] : '';
     }
-  }
+
+
+    /** @return INeevoDriver */
+    public function driver(){
+        return $this->driver;
+    }
+
+
+    /** @return NeevoStmtParser */
+    public function stmtParser(){
+        return $this->stmtParser;
+    }
+
+
+    /** @return INeevoCache */
+    public function cache(){
+        return $this->cache;
+    }
+
+
+    /**
+     * Create an alias for configuration value.
+     * @param array $config Passed by reference
+     * @param string $key
+     * @param string $alias Alias of $key
+     * @return void
+     */
+    public static function alias(&$config, $key, $alias){
+        if(isset($config[$alias]) && !isset($config[$key])){
+            $config[$key] = $config[$alias];
+            //unset($config[$alias]);
+        }
+    }
+
+
+    /**
+     * Attach given observer.
+     * @param INeevoObserver $observer
+     * @return void
+     */
+    public function attachObserver(INeevoObserver $observer){
+        $this->observers->attach($observer);
+    }
+
+
+    /**
+     * Detach given observer.
+     * @param INeevoObserver $observer
+     * @return void
+     */
+    public function detachObserver(INeevoObserver $observer){
+        $this->observers->detach($observer);
+    }
+
+
+    /**
+     * Notify observers.
+     * @param int $event
+     * @param NeevoStmtBase $statement
+     * @return void
+     */
+    public function notifyObservers($event, NeevoStmtBase $statement = null){
+        foreach($this->observers as $observer){
+            $observer->updateStatus($this, $event, $statement);
+        }
+    }
+
+
+    /*    ************    Internal methods ************    */
+
+
+    /** @internal */
+    public function realConnect(){
+        if($this->connected === false){
+            $this->driver->connect($this->config);
+            $this->connected = true;
+
+            $this->notifyObservers(INeevoObserver::CONNECT);
+        }
+    }
+
+
+    private function isDriver($class){
+        return (class_exists($class) && in_array('INeevoDriver', class_implements($class)));
+    }
+
+
+    /**
+     * Set the driver and statement parser.
+     * @param string $driver
+     * @return void
+     * @throws NeevoException
+     */
+    private function setDriver($driver){
+        $class = "NeevoDriver$driver";
+
+        if(!$this->isDriver($class)){
+            include_once dirname(__FILE__) . '/drivers/'.strtolower($driver).'.php';
+
+            if(!$this->isDriver($class)){
+                throw new NeevoException("Unable to create instance of Neevo driver '$driver'.");
+            }
+        }
+
+        $this->driver = new $class;
+
+        // Set statement parser
+        if(in_array('NeevoStmtParser', class_parents($class))){
+            $this->stmtParser = $this->driver;
+        }
+        else{
+            $this->stmtParser = new NeevoStmtParser;
+        }
+    }
+
 
 }
