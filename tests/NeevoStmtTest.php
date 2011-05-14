4051021e4a2a19a72c8656226e3688a56dfa17ab
Smasty <martin@smasty.net>
2011-05-14 13:28:22 +0200

    NeevoStmt test - remove comments, rename $c to $connection

--- tests/NeevoStmtTest.php
+++ tests/NeevoStmtTest.php
@@ -4,17 +4,17 @@ use PHPUnit_Framework_Assert as A;
 
 
 /**
- * Tests for NeevoStmtBase.
+ * Tests for NeevoStmt.
  */
 class NeevoStmtTest extends PHPUnit_Framework_TestCase {
 
 
 	/** @var NeevoConnection */
-	private $c;
+	private $connection;
 
 
 	protected function setUp(){
-		$this->c = new NeevoConnection(array(
+		$this->connection = new NeevoConnection(array(
 				'driver' => 'Dummy'
 			));
 	}
@@ -25,86 +25,60 @@ class NeevoStmtTest extends PHPUnit_Framework_TestCase {
 	}
 
 
-	/**
-	 * Test NeevoStmt::createUpdate().
-	 */
 	public function testCreateUpdate(){
-		$stmt = NeevoStmt::createUpdate($this->c, $s = 'table', $d = array('column' => 'value'));
+		$stmt = NeevoStmt::createUpdate($this->connection, $s = 'table', $d = array('column' => 'value'));
 		A::assertEquals($d, $stmt->getValues());
 		A::assertEquals($s, $stmt->getTable());
 		A::assertEquals(Neevo::STMT_UPDATE, $stmt->getType());
 	}
 
 
-	/**
-	 * Test NeevoStmt::createInsert().
-	 */
 	public function testCreateInsert(){
-		$stmt = NeevoStmt::createInsert($this->c, $s = 'table', $d = array('column' => 'value'));
+		$stmt = NeevoStmt::createInsert($this->connection, $s = 'table', $d = array('column' => 'value'));
 		A::assertEquals($d, $stmt->getValues());
 		A::assertEquals($s, $stmt->getTable());
 		A::assertEquals(Neevo::STMT_INSERT, $stmt->getType());
 	}
 
 
-	/**
-	 * Test NeevoStmt::createDelete().
-	 */
 	public function testCreateDelete(){
-		$stmt = NeevoStmt::createDelete($this->c, $s = 'table');
+		$stmt = NeevoStmt::createDelete($this->connection, $s = 'table');
 		A::assertEquals($s, $stmt->getTable());
 		A::assertEquals(Neevo::STMT_DELETE, $stmt->getType());
 	}
 
 
-	/**
-	 * Test run() method.
-	 */
 	public function testRun(){
-		$stmt = NeevoStmt::createDelete($this->c, 'table');
+		$stmt = NeevoStmt::createDelete($this->connection, 'table');
 		A::assertTrue($stmt->run());
 	}
 
 
-	/**
-	 * Test InsertId() method.
-	 */
 	public function testInsertId(){
-		$stmt = NeevoStmt::createInsert($this->c, 'table', array('column', 'value'));
+		$stmt = NeevoStmt::createInsert($this->connection, 'table', array('column', 'value'));
 		A::assertEquals(4, $stmt->insertId());
 	}
 
 
-	/**
-	 * Test InsertId() method on non-INSERT query.
-	 */
 	public function testInsertIdException(){
-		$stmt = NeevoStmt::createDelete($this->c, 'table');
+		$stmt = NeevoStmt::createDelete($this->connection, 'table');
 		try{
 			$stmt->insertId();
-			$exc = false;
 		} catch(NeevoException $e){
-			$exc = true;
+			return;
 		}
-
-		A::assertTrue($exc);
+		A::fail();
 	}
 
 
-	/**
-	 * Test affectedRows() method.
-	 */
 	public function testAffectedRows(){
-		$stmt = NeevoStmt::createDelete($this->c, 'table');
+		$stmt = NeevoStmt::createDelete($this->connection, 'table');
 		A::assertEquals(1, $stmt->affectedRows());
 	}
 
 
-	/**
-	 * Test resetState() method.
-	 */
 	public function testResetState(){
-		$stmt = NeevoStmt::createDelete($this->c, 'table');
+		$stmt = NeevoStmt::createDelete($this->connection, 'table');
 		$stmt->affectedRows();
 		$stmt->resetState();
 
