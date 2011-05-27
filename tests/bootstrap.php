5d43f7f9cd91cb71ecca7c5f6d232bb19dc09c72
Smasty <martin@smasty.net>
2011-05-27 22:15:54 +0200

    Rename /dummy-objects to /mocks

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../src/neevo.php';
 
 // Test helper objects autoloader
 spl_autoload_register(function($class){
-	$path = __DIR__ . "/dummy-objects/$class.php";
+	$path = __DIR__ . "/mocks/$class.php";
 	if(file_exists($path)){
 		return require_once $path;
 	}
