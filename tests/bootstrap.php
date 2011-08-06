cebe22b3a091118cb15fc46ff422c2c136e44985
Smasty <martin@smasty.net>
2011-08-06 21:33:35 +0200

    Revert 'tests/bootstrap.php' to 37ff1d5

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,11 +4,9 @@ require_once __DIR__ . '/../src/neevo.php';
 
 
 // Test helper objects autoloader
-function NeevoTestAutoload($class){
+spl_autoload_register(function($class){
 	$path = __DIR__ . "/mocks/$class.php";
 	if(file_exists($path))
 		return require_once $path;
 	return false;
-}
-
-spl_autoload_register('NeevoTestAutoload');
+});
