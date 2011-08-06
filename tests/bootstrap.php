18bc2fc035781ec77e7d264f35780dc733dbb94f
Smasty <martin@smasty.net>
2011-08-06 21:25:27 +0200

    NeevoLoader tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,9 +4,11 @@ require_once __DIR__ . '/../src/neevo.php';
 
 
 // Test helper objects autoloader
-spl_autoload_register(function($class){
+function NeevoTestAutoload($class){
 	$path = __DIR__ . "/mocks/$class.php";
 	if(file_exists($path))
 		return require_once $path;
 	return false;
-});
+}
+
+spl_autoload_register('NeevoTestAutoload');
