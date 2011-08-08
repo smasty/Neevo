2e92b175afd5e5a59b4900a1a6a7cfdb197eeace
Smasty <martin@smasty.net>
2011-08-08 13:42:15 +0200

    Tests for NeevoException

---
+++ tests/NeevoExceptionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+
+/**
+ * Tests for NeevoException additional features.
+ */
+class NeevoExceptionTest extends PHPUnit_Framework_TestCase {
+
+
+	public function testConstructor(){
+		$e = new NeevoException($m = 'error', $c = 0, $s = 'SELECT * FROM error', $p = new Exception);
+		$this->assertEquals($m, $e->getMessage());
+		$this->assertEquals($c, $e->getCode());
+		$this->assertEquals($p, $e->getPrevious());
+		$this->assertEquals($s, $e->getSql());
+	}
+
+
+	public function testToString(){
+		$e = new NeevoException(null, 0, $sql = 'SELECT * FROM error');
+		$this->assertContains("\nSQL: $sql", (string) $e);
+	}
+
+
+	public function testObservable(){
+		$e = new NeevoException;
+		$observer = new DummyObserver;
+
+		$e->attachObserver($observer, $event = DummyObserver::EXCEPTION);
+		$e->notifyObservers($event);
+		$this->assertTrue($observer->isNotified($firedEvent));
+		$this->assertEquals($event, $firedEvent);
+
+		$observer->reset();
+		$e->detachObserver($observer);
+		$e->notifyObservers($event);
+		$this->assertFalse($observer->isNotified());
+	}
+
+
+}
