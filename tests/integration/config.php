7c81d9ed401776a8ab68ac06f9076ba9713dff52
Smasty <martin@smasty.net>
2011-08-21 18:09:43 +0200

    Use SQLite3 :memory: database in tests

--- tests/integration/config.php
+++ tests/integration/config.php
@@ -3,6 +3,9 @@
 require __DIR__ . '/../../src/neevo.php';
 
 $db = new Neevo(array(
-	'driver' => 'sqlite',
-	'file' => __DIR__ . '/sqlite.db'
+	'driver' => 'sqlite3',
+	'file' => ':memory:'
 ));
+
+// Load database to the memory
+$db->loadFile(__DIR__ . '/db.sql');
