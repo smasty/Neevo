9e27da4e7e7b7e808c16e1f35a0d91b95c69c688
Smasty <martin@smasty.net>
2011-09-24 17:12:13 +0200

    Comment header

--- src/Neevo/Drivers/sqlite2.php
+++ src/Neevo/Drivers/sqlite2.php
@@ -1,15 +1,11 @@
 <?php
 /**
- * Neevo - Tiny open-source database abstraction layer for PHP
- *
- * Copyright 2010-2011 Martin Srank (http://smasty.net)
+ * Neevo - Tiny database layer for PHP. (http://neevo.smasty.net)
  *
  * This source file is subject to the MIT license that is bundled
  * with this package in the file license.txt.
  *
- * @author	 Martin Srank (http://smasty.net)
- * @license	http://neevo.smasty.net/license MIT license
- * @link    	 http://neevo.smasty.net/
+ * Copyright (c) 2011 Martin Srank (http://smasty.net)
  *
  */
 
