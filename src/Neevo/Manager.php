a3037d8e4d81e0977697de7a36d210dec3a4cca8
Smasty <martin@smasty.net>
2011-12-28 17:23:33 +0100

    Revert version back to 2.0-dev

--- src/Neevo/Manager.php
+++ src/Neevo/Manager.php
@@ -36,7 +36,7 @@ class Manager implements IObservable, IObserver {
 
 
 	// Neevo version
-	const VERSION = '2.0-beta',
+	const VERSION = '2.0-dev',
 	REVISION = '@VCREV@ released on @VCDATE@';
 
 	// Data types
