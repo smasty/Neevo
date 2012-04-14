5a79ba18368ef9772e04990d7b5aa9d19792abed
Smasty <martin@smasty.net>
2012-04-14 20:13:07 +0200

    Release Neevo v2.0.1

--- src/Neevo/Manager.php
+++ src/Neevo/Manager.php
@@ -36,7 +36,7 @@ class Manager implements IObservable, IObserver {
 
 
 	// Neevo version
-	const VERSION = '2.0',
+	const VERSION = '2.0.1',
 	REVISION = '@VCREV@ released on @VCDATE@';
 
 	// Data types
